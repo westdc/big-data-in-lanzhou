3cf12280f8ee744b37b907b0e23a35e4cbd6ef0a
maxd <mxd1007@163.com>
2015-09-18 14:39:07 +0800

    update post('/user/toggle') in routes/index.js

--- models/user-models.js
+++ models/user-models.js
@@ -83,10 +83,17 @@ User.count = function(callback) {
 };
 
 User.updateStatus = function(user, callback) {
+    console.log(user);
     UserModel.findOneAndUpdate({_id:user._id},{ $set: {status:user.status}}).exec(function (err, user) {
         if (err) {
             return callback(err);
         }
         callback(null,user);
+    });
+};
+
+User.remove = function(user, callback) {
+    UserModel.where({_id:user._id}).findOneAndRemove().exec(function(err, user) {
+
     });
 };
