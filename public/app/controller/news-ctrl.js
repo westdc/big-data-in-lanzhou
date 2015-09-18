d7406907f3bd86edb24d1eb994aac5cc136f7395
nober <957117295@qq.com>
2015-09-18 12:50:31 +0800

    Merge branch 'master' of https://github.com/westdc/big-data-in-lanzhou
    Conflicts:
    	views/index.ejs

--- public/app/controller/news-ctrl.js
+++ public/app/controller/news-ctrl.js
@@ -3,25 +3,25 @@
  */
 
 angular.module("technicalSalon")
-    .controller("lastNewsCtrl",function($scope,$http ,NewsService) {
-        $scope.news = NewsService.last({num:5});
+    .controller("lastNewsCtrl", function ($scope, $http, NewsService) {
+        $scope.news = NewsService.last({num: 5});
     })
-    .controller("newsCtrl",function($scope,$http,NewsService) {
+    .controller("newsCtrl", function ($scope, $http, NewsService) {
         $scope.currentPage = 1;
         $scope.totalItems = 0;
         $scope.items = [];
 
-        $http.get('/count/news').success(function(data) {
+        $http.get('/count/news').success(function (data) {
             $scope.totalItems = data.totalItems;
-            $scope.items = NewsService.query({skip:($scope.currentPage - 1)*10, pageSize:10});
+            $scope.items = NewsService.query({skip: ($scope.currentPage - 1) * 10, pageSize: 10});
         });
 
-        $scope.pageChanged = function(page) {
-            $scope.items = NewsService.query({skip:($scope.currentPage - 1)*10, pageSize:10});
+        $scope.pageChanged = function (page) {
+            $scope.items = NewsService.query({skip: ($scope.currentPage - 1) * 10, pageSize: 10});
         };
 
     })
-    .controller("newsDetailCtrl",function($scope, $routeParams, $http, NewsService){
-        var id=$routeParams.id;
-        $scope.n = NewsService.get({id:id});
+    .controller("newsDetailCtrl", function ($scope, $routeParams, $http, NewsService) {
+        var id = $routeParams.id;
+        $scope.n = NewsService.get({id: id});
     });
