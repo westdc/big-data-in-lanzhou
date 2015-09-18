e64f92b7368d2b0c2e64182b1d7897a0543cfddc
4ngle <j2eer@163.com>
2015-09-18 18:07:54 +0800

    add message

--- public/app/controller/message-ctrl.js
+++ public/app/controller/message-ctrl.js
@@ -1,5 +1,5 @@
 angular.module("technicalSalon")
-    .controller("messageCtrl", function ($scope, $http, $modal) {
+    .controller("messageCtrl", function ($scope, $http, $modal, MessageService) {
 
         $scope.items = [];
         $scope.open = function (modalCtrl, size) {
@@ -9,25 +9,28 @@ angular.module("technicalSalon")
                 size: size
             });
 
+            modalInstance.result.then(function (message) {
+                var messageService = new MessageService(message);
+                messageService.$save(function (data) {
+                    if (data.result == 'error') {
+                        $scope.$emit(data.result, data.message);
+                    } else {
+                        $scope.$emit(data.result, data.message);
+                    }
+                }, function (err) {
+                    console.log(err);
+                });
+            }, function () {
+
+            });
         }
     })
-    .controller("submitMessageCtrl", function ($scope, $modalInstance, MessageService) {
+    .controller("submitMessageCtrl", function ($scope, $modalInstance) {
 
         $scope.message = {};
 
         $scope.submit = function (message) {
-
-            var messageService = new MessageService(message);
-            messageService.$save(function (data) {
-                if (data.result == 'error') {
-                    $scope.$emit(data.result, data.message);
-                } else {
-                    $scope.$emit(data.result, data.message);
-                    $modalInstance.close();
-                }
-            }, function (err) {
-                console.log(err);
-            });
+            $modalInstance.close(message);
         };
         $scope.cancel = function () {
             $scope.message = {};
@@ -49,19 +52,38 @@ angular.module("technicalSalon")
             $scope.items = MessageService.query({skip: ($scope.currentPage - 1) * 10, pageSize: 10});
         };
 
-        $scope.openDeleteDialog = function(id, modalCtrl, size) {
+        $scope.openDeleteDialog = function(message, modalCtrl, size) {
             var modalInstance = $modal.open({
                 templateUrl: 'app/partials/admin/template/alert-delete.html',
                 controller: modalCtrl,
                 size: size,
-                scope: ''
+                resolve:{
+                    id:function() {
+                        return message._id
+                    }
+                }
             });
+
+            modalInstance.result.then(function(id) {
+                $http.post('/message/remove',{id:id}).success(function(data) {
+                    if (data.result == 'error') {
+                        $scope.$emit(data.result, data.message);
+                    } else {
+                        $scope.$emit(data.result, data.message);
+                        $scope.items.splice($scope.items.indexOf(message), 1);
+                    }
+                }).error(function(err) {
+                   console.log(err);
+                });
+            }, function() {
+
+            })
         }
 
     })
-    .controller('deleteMessageCtrl', function($scope, $modalInstance, MessageService) {
+    .controller('deleteMessageCtrl', function($scope, $modalInstance, id) {
         $scope.submit = function () {
-            $modalInstance.close();
+            $modalInstance.close(id);
         };
         $scope.cancel = function () {
             $modalInstance.dismiss('cancel');
