a5e344d15af5a0390908ff5e60a438a63a950e7b
maxd <mxd1007@163.com>
2015-09-24 17:51:42 +0800

    add passport

--- models/user-models.js
+++ models/user-models.js
@@ -5,58 +5,25 @@ var crypto = require('crypto');
 var mongoose = require('./db');
 var Schema = mongoose.Schema,
     passportLocalMongoose = require('passport-local-mongoose');
-var User = new Schema({
-    email:String,
+var UserSchema = new Schema({
     name:String,
-    password:String,
     createAt: { type: Date, default: Date.now },
     status: { type: Number, default: 1 }
 });
 
-User.plugin(passportLocalMongoose);
+UserSchema.plugin(passportLocalMongoose);
+console.log(UserSchema.email);
 
-var UserModel = mongoose.model('User', User);
+var UserModel = mongoose.model('User', UserSchema);
 module.exports.UserModel = UserModel;
 
 function User(user) {
-    this.email = user.email;
     this.name = user.name;
-    this.password = user.password;
     this.status = user.status;
 };
 
 module.exports.User = User;
 
-//存储用户信息
-//User.prototype.save = function(callback) {
-//    var md5 = crypto.createHash('md5');
-//
-//    var user = {
-//        email: this.email,
-//        name: this.name,
-//        password: md5.update(this.password).digest('hex'),
-//        status: 1
-//    };
-//
-//     console.log(user);
-//    //打开数据库
-//    var newUser = new UserModel(user);
-//
-//    newUser.save(function(err, user){
-//        if(err){
-//            return callback(err);
-//        }
-//        callback(null, user);
-//    });
-//};
-//
-//User.authenticate = function(p,password) {
-//    var md5 = crypto.createHash('md5');
-//
-//    return p == md5.update(password).digest('hex');
-//}
-
-//读取用户信息
 User.get = function(email, callback) {
     UserModel.findOne({email:email}, function (err, user)
     {
