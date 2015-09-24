0ef1a753f71130604eb16c05314205c5e908cbe8
maxd <mxd1007@163.com>
2015-09-24 18:28:02 +0800

    add passport

--- routes/index-router.js
+++ routes/index-router.js
@@ -140,7 +140,7 @@ router.get('/news/:id',function(req,res){
 });
 
 router.post('/news/update',function(req,res) {
-    News.update(req.body.id, req.body.title, req.body.name, req.body.content, function (err) {
+    News.update(req.body.news, function (err) {
         if (err) {
             return res.jsonp({result: 'error', message: "修改新闻失败"});
         } else {
